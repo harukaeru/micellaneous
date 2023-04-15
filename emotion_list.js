b013f8b99e6c8a3a0c933a30a0230f39ece8c94e
harukaeru <nekoneko.myaomyao@gmail.com>
2023-04-16 03:35:24 +0900

    emotion

---
+++ emotion_list.js
@@ -0,0 +1,250 @@
+data = [
+  {
+    "jp": "狂喜",
+    "en": "ECSTASY",
+    "description": "非常に大きな幸福の状態"
+  },
+  {
+    "jp": "興奮",
+    "en": "EXCITEMENT",
+    "description": "強力な熱意"
+  },
+  {
+    "jp": "驚嘆",
+    "en": "WONDER",
+    "description": "非常に大きな驚きや、信じがたい経験"
+  },
+  {
+    "jp": "ナチェス",
+    "en": "NACHES",
+    "description": "自分の子供や教え子が成長した時の誇らしい喜び"
+  },
+  {
+    "jp": "フィエロ",
+    "en": "FIERO",
+    "description": "困難な挑戦を乗り越えた時の喜び"
+  },
+  {
+    "jp": "高慢",
+    "en": "PRIDE",
+    "description": "自分自身の成果や、仲間の成果から得た深い喜び"
+  },
+  {
+    "jp": "平穏",
+    "en": "PEACE",
+    "description": "穏やかであることの喜び"
+  },
+  {
+    "jp": "安心",
+    "en": "RELIEF",
+    "description": "不快であることがなくなった時の心の落ち着き"
+  },
+  {
+    "jp": "シャーデンフロイデ",
+    "en": "SCHADENFREUDE",
+    "description": "他人やライバルの不幸を楽しむ気持ち"
+  },
+  {
+    "jp": "面白い",
+    "en": "AMUSEMENT",
+    "description": "遊び心のある、明るく楽しい気持ち"
+  },
+  {
+    "jp": "同情",
+    "en": "COMPASSION/JOY",
+    "description": "他人の苦しみを和らげることで得られる喜びの気持ち"
+  },
+  {
+    "jp": "喜び",
+    "en": "REJOICING",
+    "description": "優しさや思いやりを見たときに経験する、暖かく爽やかな気持ち"
+  },
+  {
+    "jp": "感覚的快楽",
+    "en": "SENSORY",
+    "description": "視覚、聴覚、触覚、味覚、嗅覚の5つの感覚のうち、1つを通して得られた時の楽しさ"
+  },
+  {
+    "jp": "強い嫌悪",
+    "en": "LOATHING",
+    "description": "他人に向けた激しい嫌悪感、または自分自身に向けた自己嫌悪"
+  },
+  {
+    "jp": "憎悪",
+    "en": "ABHORRENCE",
+    "description": "強い嫌悪と憎しみが混ざり合った気持ち"
+  },
+  {
+    "jp": "反感",
+    "en": "REVULSION",
+    "description": "嫌悪感と憎しみが混ざり合った気持ち"
+  },
+  {
+    "jp": "嫌気",
+    "en": "REPUGNANCE",
+    "description": "コンセプトやアイデアに対する強い嫌悪感"
+  },
+  {
+    "jp": "嫌悪",
+    "en": "DISTASTE",
+    "description": "悪い味、匂い、物質、またはアイデアへの嫌悪感"
+  },
+  {
+    "jp": "嫌い",
+    "en": "AVERSION",
+    "description": "嫌いなことを避けるための衝動"
+  },
+  {
+    "jp": "苦手",
+    "en": "DISLIKE",
+    "description": "自分にとって得意ではない趣好"
+  },
+  {
+    "jp": "苦悩",
+    "en": "ANGUISH",
+    "description": "激しい悲しみや苦しみ"
+  },
+  {
+    "jp": "悲嘆",
+    "en": "SORROW",
+    "description": "苦痛と苦しみの感覚、多くの場合は喪失が原因"
+  },
+  {
+    "jp": "悲哀",
+    "en": "GRIEF",
+    "description": "深い喪失の悲しみ"
+  },
+  {
+    "jp": "絶望",
+    "en": "DESPAIR",
+    "description": "悪い状況が変わるという希望の喪失"
+  },
+  {
+    "jp": "悲惨",
+    "en": "MISERY",
+    "description": "強い苦しみや不幸の気持ち"
+  },
+  {
+    "jp": "落胆",
+    "en": "HOPELESSNESS",
+    "description": "良いことは何も起こらないという信念"
+  },
+  {
+    "jp": "無力",
+    "en": "HELPLESSNESS",
+    "description": "状況が改善できないという認識"
+  },
+  {
+    "jp": "諦め",
+    "en": "RESIGNATION",
+    "description": "何もできないという信念"
+  },
+  {
+    "jp": "逸脱",
+    "en": "DISTRAUGHTNESS",
+    "description": "考えることを避けたくなる悲しみ"
+  },
+  {
+    "jp": "挫折",
+    "en": "DISCOURAGEMENT",
+    "description": "失敗を繰り返して、できないと感じる信念"
+  },
+  {
+    "jp": "残念",
+    "en": "DISAPPOINTMENT",
+    "description": "期待が満たされていない感情"
+  },
+  {
+    "jp": "震駭",
+    "en": "TERROR",
+    "description": "激しい恐怖"
+  },
+  {
+    "jp": "恐怖",
+    "en": "HORROR",
+    "description": "嫌悪感とショックの混在"
+  },
+  {
+    "jp": "パニック",
+    "en": "PANIC",
+    "description": "コントロールができない突然の恐怖"
+  },
+  {
+    "jp": "自暴自棄",
+    "en": "DESPERATION",
+    "description": "危険を減らすことができない、やけくそな感情"
+  },
+  {
+    "jp": "恐れる",
+    "en": "DREAD",
+    "description": "重大な危険を予期した感情"
+  },
+  {
+    "jp": "不安",
+    "en": "ANXIETY",
+    "description": "訪れるかもしれない脅威への恐怖"
+  },
+  {
+    "jp": "緊張感",
+    "en": "NERVOUSNESS",
+    "description": "危険があるかどうかわからない不確実な感情"
+  },
+  {
+    "jp": "狼狽",
+    "en": "TREPIDATION",
+    "description": "危険の可能性を予期した、うろたえる感情"
+  },
+  {
+    "jp": "憤激",
+    "en": "FURY",
+    "description": "制御できない暴力的な怒り"
+  },
+  {
+    "jp": "執念",
+    "en": "VENGEFULNESS",
+    "description": "傷つけられたことに対して復讐したいと考える欲望"
+  },
+  {
+    "jp": "怨み",
+    "en": "BITTERNESS",
+    "description": "不当な扱いを受けた時の怒り"
+  },
+  {
+    "jp": "論争性",
+    "en": "ARGUMENTATIVENESS",
+    "description": "違う意見に対して反抗する気持ち"
+  },
+  {
+    "jp": "激昂",
+    "en": "EXASPERATION",
+    "description": "繰り返されるか、強い迷惑によって引き起こされる怒り"
+  },
+  {
+    "jp": "フラストレーション",
+    "en": "FRUSTRATION",
+    "description": "欲求が満たされない時の苛立ち"
+  },
+  {
+    "jp": "苛立ち",
+    "en": "ANNOYANCE",
+    "description": "ちょっとした迷惑や不便さに対する軽い怒り"
+  }
+]
+
+function convertToMarkdownTable(data) {
+  let markdownTable = "感情名 |  説明\n---|---|---\n";
+
+  for (const item of data) {
+    const jp = item.jp;
+    const en = item.en;
+    const description = item.description;
+
+    markdownTable += `${jp} | ${description}\n`;
+  }
+
+  return markdownTable;
+}
+
+const markdownTable = convertToMarkdownTable(data);
+console.log(markdownTable);
+
